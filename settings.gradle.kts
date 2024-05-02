04f3e55340a8d7b3fb69a7f6d98a87d817a9e4a2
mohamedtahamaxab <mohamed.omran@maxab.io>
2024-05-03 02:42:11 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UniversityTask"
+include(":app")
+include(":app:modulea")
